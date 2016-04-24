5d5d040aa597c3dcbc51fd3077a841a5d6d8d01d
Roger A <rogeragc@gmail.com>
2016-04-23 22:01:54 -0700

    commented out local test code

--- static/map.js
+++ static/map.js
@@ -1,11 +1,10 @@
 function populateMap() {
     var appUrl = 'http://ghostgearrecon.us/equipment/list';
-    var data = {equipment: [{latitude: "37.77740", longitude: "-122.40805", retrieved: "false", uuid:"577ba35b-8ff1-42ec-827e-40dcfa3bc2b6"},
-                            {latitude: "37.77740", longitude: "-122.40805", retrieved: "false", uuid:"c995a8a4-5de5-42c8-b57c-eec498be56dc"}
-    ]};
+//    var data = {equipment: [{latitude: "37.77740", longitude: "-122.40805", retrieved: "false", uuid:"577ba35b-8ff1-42ec-827e-40dcfa3bc2b6"},
+//                            {latitude: "37.77740", longitude: "-122.40805", retrieved: "false", uuid:"c995a8a4-5de5-42c8-b57c-eec498be56dc"}
+//    ]};
     
-    //$.get(appUrl, function(data) {
-        console.log(data);
+    $.get(appUrl, function(data) {
         L.mapbox.accessToken = 'pk.eyJ1IjoiZWNvYW5kcmV3dHJjIiwiYSI6InZ0MzhibUkifQ.Q0qYLMThqHqmAJ3q3dxvog';
         var map = L.mapbox.map('map', 'mapbox.streets').setView([37.5871329,-121.9321825], 9);
         if (data) {
@@ -20,7 +19,7 @@ function populateMap() {
             	}).addTo(map);
             }
         }
-    //});
+    });
         
 
     $("#changeView").on("click", function() {
